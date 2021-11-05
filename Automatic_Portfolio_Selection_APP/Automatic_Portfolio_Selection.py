fd473cea4cc8928e0a09f97ab9b9267ae354c118
Shasha <shasha867619.sl@gmail.com>
2021-11-04 20:15:35 -0400

    integrate a new feature of initial investment

--- Automatic_Portfolio_Selection_APP/Automatic_Portfolio_Selection.py
+++ Automatic_Portfolio_Selection_APP/Automatic_Portfolio_Selection.py
@@ -1,4 +1,4 @@
-# A Database CLI Application
+# An Automatic Customized Portfolio Construction CLI Application
 
 # Import modules
 import pandas as pd
@@ -7,11 +7,8 @@ import os
 import alpaca_trade_api as tradeapi
 from dotenv import load_dotenv
 import questionary
-import requests
-import json
 from MCForecastTools import MCSimulation
 import matplotlib.pyplot as plt
-
 
 
 # Load .env file
@@ -22,12 +19,15 @@ alpaca_api_key = os.getenv("ALPACA_API_KEY")
 alpaca_secret_key = os.getenv("ALPACA_SECRET_KEY")
 
 
-# Create a function called `Portfolio_selection` that will select portfolio for investors who are in the different level of risk-averse.
+# Create a function called `Portfolio_selection` that will construct a portfolio based on users' weights
 # This function will be called from the `__main__` loop.
 
 def portoflio_selection(customer_bond_weight, customer_stock_weight, customer_initial_investment):
 
-    # Using questionary, ask the investor what is his/her risk-aversion level 
+
+    ###Part 1: Prepare the data
+
+    #Change the users' inputs to float and append to the weight list
     customer_bond_weight = float(customer_bond_weight)
     customer_stock_weight = float(customer_stock_weight)
     customber_choose_weight = []
@@ -36,12 +36,13 @@ def portoflio_selection(customer_bond_weight, customer_stock_weight, customer_in
 
     print("Running report ...")
     
-    #Get API data
+    #Setup API 
     alpaca = tradeapi.REST(
     alpaca_api_key,
     alpaca_secret_key,
     api_version = "v2")
     
+    #Setup parameters for API Data
     tickers = ["SPY", "AGG"]
 
     timeframe = "1D"
@@ -49,7 +50,7 @@ def portoflio_selection(customer_bond_weight, customer_stock_weight, customer_in
     start_date = pd.Timestamp("2019-01-02", tz = "America/New_York").isoformat()
     end_date = pd.Timestamp("2021-10-31", tz = "America/New_York").isoformat()
     
-    
+    #Get API Data in a dataframe
     prices_df = alpaca.get_barset(
     tickers,
     timeframe,
@@ -57,9 +58,11 @@ def portoflio_selection(customer_bond_weight, customer_stock_weight, customer_in
     end = end_date
     ).df
     
+
+
+    ###Part 2:Run the MC simulation
     
-    
-    #Run the MC simulation
+    #set up MC simulation
     MC_weight = MCSimulation(
       portfolio_data = prices_df,
       weights = customber_choose_weight,
@@ -68,21 +71,19 @@ def portoflio_selection(customer_bond_weight, customer_stock_weight, customer_in
     
     MC_weight.calc_cumulative_return()
     
-    
-    MC_weight_line_plot = MC_weight.plot_simulation()
-    
-    
-    MC_weight_distribution_plot = MC_weight.plot_distribution()
-    
-    
     MC_weight_table = MC_weight.summarize_cumulative_return()
-    
-    
-   #Visuallation
 
+
+    #Display the simulation result
+    print("\n......Your Simulated Portfolio Results.....\n")
+    print (MC_weight_table)
+    
+
+
+
+    ###Part 3:Visuallation
 
     #Print the simulated portfolio allocation
-
     portfolio_allocation = customber_choose_weight
     portfolio_labels = ["AGG", "SPY"]
 
@@ -91,45 +92,49 @@ def portoflio_selection(customer_bond_weight, customer_stock_weight, customer_in
     plt.show()
     
 
-
+    #Prepare the simulated returns data
     simulated_returns_data = {
     "mean": list(MC_weight.simulated_return.mean(axis=1)),
     "median": list(MC_weight.simulated_return.median(axis=1)),
     "min": list(MC_weight.simulated_return.min(axis=1)),
     "max": list(MC_weight.simulated_return.max(axis=1))}
     
-    
     df_simulated_returns = pd.DataFrame(simulated_returns_data)
 
-    plt.title("Simulated Daily Returns Behavior of simulated portfolio Over the Next 3 Years: Max, Min, Mean, and Median")
+
+    #Print the simulated daily returns
+    plt.title("Simulated Daily Returns Behaviors of Your Portfolio Over the Next 3 Years: Max, Min, Mean, and Median")
     plt.plot(df_simulated_returns)
     plt.show()
     
 
+    #Print the simulated investment result
     customer_initial_investment = float(customer_initial_investment)
     cumulative_pnl = customer_initial_investment * df_simulated_returns
 
-    plt.title("The Result of Initial Investment to the Simulated Portfolio Over the Next 3 Years: Max, Min, Mean, and Median")
+    plt.title(f"The Simulated Result of Initial Investment of ${customer_initial_investment} to Your Portfolio Over the Next 3 Years: Max, Min, Mean, and Median")
     plt.plot(cumulative_pnl)
     plt.show()
     
 
+    #Calculate the results of simulation portfolio
     ci_lower_three_cumulative_return = MC_weight_table[8] * customer_initial_investment
     ci_upper_three_cumulative_return = MC_weight_table[9] * customer_initial_investment
 
+    #Suppose the risk-free rate is 2%
     risk_free_rate = 0.02
 
     sharpe_ratio = (MC_weight_table[1] - risk_free_rate) / MC_weight_table[2]
     
     # Create a statement that displays the `results` of simulated portfolio calculation.
     # On a separate line (\n) ask the use if they would like to continue running the application.
-    results = f"There is a 95% chance that an initial investment of ${customer_initial_investment} in the simulated stock and bond portfolio over the next 3 years will end within in the range of ${ci_lower_three_cumulative_return: .2f} and ${ci_upper_three_cumulative_return: .2f}. The Sharpe ratio of the simulated portfolio is {sharpe_ratio: .2f}"
+    results = f"There is a 95% chance that an initial investment of ${customer_initial_investment} in the simulated stock and bond portfolio over the next 3 years will end within in the range of ${ci_lower_three_cumulative_return: .2f} and ${ci_upper_three_cumulative_return: .2f}. The Sharpe ratio of your simulated portfolio is {sharpe_ratio: .2f}"
     
     # Using the `results` statement created above,
     # prompt the user to run the report again (`y`) or exit the program (`n`).
     continue_running = questionary.select(results, choices=['y', 'n']).ask()
 
-    # Return the `continue_running` variable from the `sector_report` function
+    # Return the `continue_running` variable from the `portoflio_selection` function
     return continue_running
 
 
@@ -144,14 +149,15 @@ if __name__ == "__main__":
 
     print("The portfolio will be constructed based on your choice\n")
 
-    print("We will forcast and display the result of your simulated portfolio in the next 3 years through Monte Carlo Simulation\n")
+    print("We will forecast and display the result of your simulated portfolio in the next 3 years through Monte Carlo Simulation\n")
 
     print("\n......Instruction.....\n")
 
     print("First Step: Please enter your desired portion of bond in your portfolio. For example, 40% is to input .40\n")
     print("Second Step: Please enter your desired portion of stock in your portfolio. For example, 60% is to input .60\n")
     print("REMEMBER: The numbers entered in the first step and the second step should be added equal to 1. \n")
-    
+    print("Third Step: Please enter your initial investment amount: $\n")
+
     # Let the users customize their portfolios with Bond/Stock weights     
     customer_bond_weight = questionary.text("What's your desired weight of Bond in the portfolio?").ask()
     customer_stock_weight = questionary.text("What's your desired weight of Stock in the portfolio?").ask()
@@ -160,8 +166,8 @@ if __name__ == "__main__":
     # Create a variable named running and set it to True
     running = True
 
-    # While running is `True` call the `sector_report` function.
-    # Pass the `nyse_df` DataFrame `sectors` and the database `engine` as parameters.
+    # While running is `True` call the `portoflio_selection` function.
+    # Pass the "customer_bond_weight" and "customer_stock_weight" as parameters.
     while running:
         continue_running = portoflio_selection(customer_bond_weight, customer_stock_weight, customer_initial_investment)
         if continue_running == 'y':
